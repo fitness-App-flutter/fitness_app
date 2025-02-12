702985f7e35a76d4f08930bbb3b1a2e8df02e0cb
yasmeen yaser <yasmeenyasser701@gmail.com.com>
2025-02-12 14:13:11 +0200

    my first commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-
-      home: Scaffold(),
-    );
+    return const MaterialApp();
   }
 }
