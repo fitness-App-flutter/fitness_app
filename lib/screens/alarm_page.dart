4074b42281a929cf36ec2dd70e4424dd816b3db6
Mahmoud Elgorf <gorf2004@gmail.com>
2025-05-08 22:40:20 +0300

    editing sleep schedule

---
+++ lib/screens/alarm_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AlarmPage extends StatelessWidget {
+  const AlarmPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Alarm"),
+      ),
+    );
+  }
+}
